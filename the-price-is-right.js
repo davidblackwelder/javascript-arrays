bfb830a0120724c3ff6f19fa63718fc6cec44756
David Blackwelder <dblackw2@gmail.com>
2018-07-24 19:19:33 -0400

    Add The Price Is Right programming quiz

---
+++ the-price-is-right.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ * 
+ * Directions:
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+Afterwards, print out the prices array to the console.
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+
+prices[0] = 2.31; // changes 1.23
+prices[2] = 11.90; // changes 90.11
+prices[6] = 0.01; // 1.10
+
+console.log(prices);
